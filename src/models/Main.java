3d15c4e91433acabe35eb6a1b43395cbf6b039e8
Timo Strating <timostrating@home.nl>
2018-01-16 11:55:34 +0100

    first layout

--- src/models/Main.java
+++ src/models/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package models;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../models/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
