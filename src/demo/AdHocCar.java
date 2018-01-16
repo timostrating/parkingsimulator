34e0bac8ddfe619d38aed1df51b8ba29db6d8254
Timo Strating <timostrating@home.nl>
2018-01-16 15:13:15 +0100

    GUI inports fxml now

--- src/demo/AdHocCar.java
+++ src/demo/AdHocCar.java
@@ -1,17 +1,20 @@
+package demo;
+
 import java.util.Random;
 import java.awt.*;
 
-public class ParkingPassCar extends Car {
-	private static final Color COLOR=Color.blue;
+public class AdHocCar extends Car {
+	private static final Color COLOR=Color.red;
 	
-    public ParkingPassCar() {
+    public AdHocCar() {
     	Random random = new Random();
     	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
         this.setMinutesLeft(stayMinutes);
-        this.setHasToPay(false);
+        this.setHasToPay(true);
     }
     
     public Color getColor(){
     	return COLOR;
     }
 }
+//hoi
--- src/demo/CarQueue.java
+++ src/demo/CarQueue.java
@@ -1,3 +1,5 @@
+package demo;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
