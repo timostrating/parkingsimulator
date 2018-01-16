edbf55bd0a87d47a6e89537419a55284c2147a76
kokhals <hilkojj@outlook.com>
2018-01-16 10:38:06 +0100

    MVC packages

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,11 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../models/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
