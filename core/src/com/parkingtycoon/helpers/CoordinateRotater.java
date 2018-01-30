7bdd298a903c9f761d03cf78fe360c36f53de5a1
hilkojj <hilkojj@outlook.com>
2018-01-30 12:15:59 +0100

    best bugfix ever
    took me 2 hours.

--- core/src/com/parkingtycoon/helpers/CoordinateRotater.java
+++ core/src/com/parkingtycoon/helpers/CoordinateRotater.java
@@ -7,11 +7,11 @@ public class CoordinateRotater {
             case 0:
                 return i;
             case 1:
-                return j;
+                return jLength - j - 1;
             case 2:
                 return iLength - i - 1;
             case 3:
-                return jLength - j - 1;
+                return j;
         }
         return 0;
     }
