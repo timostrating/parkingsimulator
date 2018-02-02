aeae913bac9064ac0b3d2698dc15425957f51611
Timo Strating <timostrating@home.nl>
2018-02-02 16:29:50 +0100

    created a fixedRingArray

--- core/src/com/parkingtycoon/helpers/FixedRingArray.java
+++ core/src/com/parkingtycoon/helpers/FixedRingArray.java
@@ -11,6 +11,8 @@ import java.util.Arrays;
  */
 public class FixedRingArray {
 
+    public boolean debug = false;
+
     private int head = 0;
     private int tail = 0;
     private float[] elements;
@@ -31,7 +33,7 @@ public class FixedRingArray {
     public void put(float value) {
         if (hasBeanRound) {
             if ((tail) % (elements.length) == head) {
-                head++;}}
+                head = (head + 1) % elements.length; }}
         else if ((tail+1) % (elements.length) == 0) {
             hasBeanRound = true; }
 
@@ -39,19 +41,54 @@ public class FixedRingArray {
         tail %= elements.length;
 
 
-        test(value);
+        if (debug) test(value);
     }
 
     public float get(int index) {
         return elements[(head + index) % elements.length];
     }
 
-    public void test(float value) {
-        System.out.println("hasBeanRound: "+hasBeanRound+"\t\t value: "+value+"\t\t tail: "+tail + "\t\t head: "+head +"\t"+toString());
-    }
-
     @Override
     public String toString() {
         return Arrays.toString(elements);
     }
+
+
+    /**
+     * This is a test to visually show how the FixedRingArray works
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        FixedRingArray queue = new FixedRingArray(5);
+        queue.debug = true;
+
+        queue.put(10f);
+        queue.put(11f);
+        queue.put(12f);
+        queue.put(13f); //
+        queue.put(14f);
+        queue.put(15f);
+        queue.put(16f);
+        queue.put(17f);
+        queue.put(18f);
+        queue.put(19f);
+        queue.put(20f);
+        queue.put(21f);
+        queue.put(22f);
+        queue.put(23f);
+        queue.put(24f);
+        queue.put(25f);
+
+        System.out.println("");
+
+        for (int i=0; i<queue.size(); i++) {
+            System.out.println(queue.get(i));
+        }
+    }
+
+    public void test(float value) {
+        System.out.println("hasBeanRound: "+hasBeanRound+"\t\t value: "+value+"\t\t tail: "+tail + "\t\t head: "+head +"\t"+toString());
+    }
+
 }
