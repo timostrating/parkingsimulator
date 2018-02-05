561f16884f0f6b0a08bd0ec25dc9c17bb62efcbc
Timo Strating <timostrating@home.nl>
2018-02-05 23:34:44 +0100

    added javadoc to the interfaces

--- core/src/com/parkingtycoon/helpers/interfaces/Showable.java
+++ core/src/com/parkingtycoon/helpers/interfaces/Showable.java
@@ -4,11 +4,26 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 
 
 /**
+ * This Class is responsible for providing a basic interface for items who would like to be showable in the UI.
  *
+ * A scenario where you would use this is a button that opens a window.
+ * The window implements Showable and by doing this it also makes the decision if it allows multiple window
+ * open at the same time.
+ *
+ * @author Timo Strating
  */
 public interface Showable {
 
+    /**
+     * Show this Item
+     *
+     * @param stage the stage where the showed item should be added to
+     */
     void show(Stage stage);
+
+    /**
+     * Hide this item. You can only hide things if you can also show them. There is no hide without a show.
+     */
     default void hide() { }  // This method is not directly required.
 
 }
